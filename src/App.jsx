40a84211cb1929bf1b411c08213bca2dcc10c1cb
ayanmirza99 <ayanjawed.m@gmail.com>
2025-07-29 01:52:15 +0500

    added layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Layout from "./Layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <LandingPage />
+      <Routes>
+        <Route path="*" element={<Navigate to={`/`} replace />} />
+        <Route element={<Layout />}>
+          <Route index element={<LandingPage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,56 +1,15 @@
-import { useEffect, useRef } from "react";
 import HeroSection from "../components/HeroSection";
-import Navbar from "../components/Navbar";
 import AboutSection from "../components/AboutSection";
 import Slider from "../components/imageSlider/Slider";
 import Services from "../components/Services";
 
 const LandingPage = () => {
-  const scrollRef = useRef(null); // for main scroll container
-  const locoScrollInstance = useRef(null); // to store Locomotive instance
-
-  useEffect(() => {
-    let scroll;
-
-    const initScroll = async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      scroll = new LocomotiveScroll({
-        el: scrollRef.current,
-        smooth: true,
-      });
-      console.log("Locomotive Scroll initialized");
-
-      locoScrollInstance.current = scroll;
-    };
-
-    initScroll();
-
-    // Optional: cleanup on unmount
-    return () => {
-      if (locoScrollInstance.current) {
-        locoScrollInstance.current.destroy();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    // Update scroll after AboutSection mounts
-    if (locoScrollInstance.current) {
-      setTimeout(() => {
-        locoScrollInstance.current.update();
-      }, 500); // slight delay to ensure DOM is ready
-    }
-  });
-
   return (
     <>
-      <Navbar />
-      <div id="main" data-scroll-container ref={scrollRef}>
-        <HeroSection />
-        <AboutSection />
-        <Slider />
-        <Services />
-      </div>
+      <HeroSection />
+      <AboutSection />
+      <Slider />
+      <Services />
     </>
   );
 };
